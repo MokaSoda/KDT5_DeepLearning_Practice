afe5057421db80754a51a63cffc69789cf43e533
KMS <kms01983@hotmail.com>
2024-03-14 17:31:05 +0900

    0314 Upload

--- DAY02/test.py
+++ DAY02/test.py
@@ -1,5 +1,8 @@
 import torch
 import torchvision.models as models
+import time
+
+
 
 model = models.resnet50(weights='ResNet50_Weights.DEFAULT')
 model.eval()
@@ -9,8 +12,9 @@ data = torch.rand(128, 3, 224, 224)
 import intel_extension_for_pytorch as ipex
 model = ipex.optimize(model, dtype=torch.bfloat16)
 ######################################################  # noqa F401
-
+start = time.time()
 with torch.no_grad(), torch.cpu.amp.autocast():
     model(data)
 
+print(f"Elapsed time: {(time.time() - start) * 1000:.2f} ms")
 print("Execution finished")
